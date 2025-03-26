27254eff8429adf79fcb541ba870be420aa15476
Novelya Putri Ramadhani <114747134+novelxv@users.noreply.github.com>
2025-03-26 17:00:20 +0700

    feat: add Layer class

---
+++ src/layers/layer.py
@@ -0,0 +1,21 @@
+import numpy as np
+from src.utils.initializer import initialize_weights
+
+class Layer:
+    def __init__(self, input_size, output_size, init_method="uniform", init_params={}):
+        self.input_size = input_size
+        self.output_size = output_size
+
+        self.weights = initialize_weights(init_method, (output_size, input_size), **init_params)
+        self.bias = initialize_weights(init_method, (output_size, 1), **init_params)
+
+        self.z = None
+        self.a = None
+
+        self.grad_weights = np.zeros_like(self.weights)
+        self.grad_bias = np.zeros_like(self.bias)
+
+    def forward(self, input):
+        self.input = input
+        self.z = np.dot(self.weights, input) + self.bias
+        return self.z
---
+++ src/utils/initializer.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def initialize_weights(method, shape, **kwargs):
+    seed = kwargs.get("seed", None)
+    if seed is not None:
+        np.random.seed(seed)
+    if method == "zero":
+        return np.zeros(shape)
+    elif method == "uniform":
+        low = kwargs.get("lower", -1.0)
+        high = kwargs.get("upper", 1.0)
+        return np.random.uniform(low, high, shape)
+    elif method == "normal":
+        mean = kwargs.get("mean", 0.0)
+        var = kwargs.get("variance", 1.0)
+        std = np.sqrt(var)
+        return np.random.normal(mean, std, shape)
+    else:
+        raise ValueError(f"Unknown initialization method: {method}")
